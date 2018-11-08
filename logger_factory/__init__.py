6d4b0b5e22bff5e0ae1568c987957aea98e0aea0
Q.S. Wang <qiang@flamingo.com>
2018-11-08 15:00:59 +1100

    Split the config logic, and the create logic

--- logger_factory/__init__.py
+++ logger_factory/__init__.py
@@ -2,13 +2,15 @@ import logging
 import os
 from logging.config import fileConfig
 
-def create_logger(name, config_file=None):
-    if (config_file not None) and (os.path.exists(os.path.abspath(config_file)):
-        fileConfig('logging_config.ini')
+def config_logger(config_file=None):
+    if (config_file is not None):
+        fileConfig('logging_config.ini', disable_existing_loggers=False)
     else:
         # default config
         logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"),
-                    format="[%(asctime)s - %(filename)s:%(lineno)s - %(funcName)s()]-12s %(levelname)-8s %(message)s",
-                    datefmt="%m-%d %H:%M")
+                            format="[%(asctime)s - %(filename)s:%(lineno)s - %(funcName)s()]-12s %(levelname)-8s %(message)s",
+                            datefmt="%m-%d %H:%M")
 
+
+def create_logger(name):
     return logging.getLogger(name)
--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(name='logger_factory',
       version='0.1',
-      description='Provides a way of create the logger, and config it',
-      url='http://github.com/qs-wang/logger_factory',
+      description='Provides a simple way of create the logger',
+      url='https://github.com/qs-wang/logger_factory',
       author='Q.S. Wang',
       author_email='wangqs_eclipse@yahoo.com',
       license='MIT',
